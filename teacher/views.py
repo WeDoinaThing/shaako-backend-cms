bc0bf71b247d4ad80f19d8a1f9ea535e7f61f058
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-01-17 09:54:35 +0600

    Edit Campaign and Edit Content

--- teacher/views.py
+++ teacher/views.py
@@ -45,10 +45,76 @@ def ngo_admin_home(request):
 def edit_content(request):
     if request.method == "POST":
         print(request.POST.get("id"))
+        print(request.POST.get("title"))
+        print(request.POST.get("details"))
+
+        selectedCampaign = Content.objects.get(id=request.POST.get("id"))
+        date = str(datetime.datetime.now().strftime("%Y-%m-%d"))
+
     return render(
         request,
         "teacher/ngo_admin_edit_content.html",
+        {
+            "entry": selectedCampaign,
+            "current_date": date,
+        },
     )
+
+def update_content(request):
+    failed = False
+    success = False
+    if request.method == "POST":
+        title = request.POST.get("inputContentTitle")
+        url = request.POST.get("inputContentLink")
+        details = request.POST.get("inputCampaignDetails")
+        date = request.POST.get("inputDate")
+        
+        print(request.POST.get("id"))
+        print("Haha")
+
+        selectedCampaign = Content.objects.get(id=request.POST.get("id"))
+        try:
+            selectedCampaign.title = title
+            selectedCampaign.associated_link = url
+            selectedCampaign.details = details
+            selectedCampaign.date = date
+
+            selectedCampaign.save()
+            success = True
+            failed = False
+
+        except Exception as e:
+            print(e)
+            success = False
+            failed = True
+
+    if request.user.is_authenticated:
+
+        ngo_admin = NGO_Admin.objects.get(user=request.user)
+        contents = Content.objects.filter(added_by=ngo_admin)
+
+        campaignDeadlineOver = Content.objects.filter(
+            added_by=ngo_admin,
+            date__lt=datetime.datetime.now().date()
+        )
+
+        campaignDeadlineFuture = Content.objects.filter(
+            added_by=ngo_admin,
+            date__gte=datetime.datetime.now().date()
+        )
+
+        return render(
+            request,
+            "teacher/ngo_admin_dashboard.html",
+            {
+                "is_ngo_admin": True,
+                "future_campaign": campaignDeadlineFuture,
+                "past_campagin": campaignDeadlineOver,
+            },
+        )
+    else:
+        return redirect("login")
+
 
 def create_content(request):
     failed = False
@@ -118,13 +184,68 @@ def ngo_admin_chw(request):
 def edit_chw(request):
     if request.method == "POST":
         print(request.POST.get("id"))
+        print(request.POST.get("name"))
+        print(request.POST.get("age"))
+        print(request.POST.get("region"))
+
+
+        selectedCHW = CHW.objects.get(id=request.POST.get("id"))
+        date = str(datetime.datetime.now().strftime("%Y-%m-%d"))
+
+        print(selectedCHW.name)
     return render(
         request,
         "teacher/ngo_admin_edit_chw.html",
         {
-            "is_ngo_admin": True,
-        }
+            "entry": selectedCHW,
+            "current_date": date,
+        },
     )
+
+
+def update_chw(request):
+    failed = False
+    success = False
+    if request.method == "POST":
+        id = request.POST.get("id")
+        name = request.POST.get("inputName")
+        age = request.POST.get("inputAge")
+        region = request.POST.get("inputRegion")
+
+        print(request.POST.get("id"))
+        print("Haha")
+
+        selectedCHW = CHW.objects.get(id=request.POST.get("id"))
+        try:
+            selectedCHW.id = id
+            selectedCHW.name = name
+            selectedCHW.age = age
+            selectedCHW.region = region
+
+            selectedCHW.save()
+            success = True
+            failed = False
+
+        except Exception as e:
+            print(e)
+            success = False
+            failed = True
+
+    if request.user.is_authenticated:
+
+        ngo_admin = NGO_Admin.objects.get(user=request.user)
+        chws = CHW.objects.filter(addedBy=ngo_admin)
+
+        return render(
+            request,
+            "teacher/ngo_admin_chw.html",
+            {
+                "is_ngo_admin": True,
+                "health_workers": chws,
+            },
+        )
+    else:
+        return redirect("login")
 
 def create_chw(request):
     failed = False
@@ -170,6 +291,7 @@ def create_chw(request):
         )
 
     if request.user.is_authenticated:
+
         year = str(datetime.datetime.now().year)
         date = str(datetime.datetime.now().strftime("%Y-%m-%d"))
         return render(
@@ -185,14 +307,6 @@ def create_chw(request):
         )
     else:
         return redirect("login")
-
-
-def super_admin_home(request):
-    if request.user.is_authenticated and request.user.is_superadmin:
-        return render(
-            request,
-            "teacher/super_admin_dashboard.html"
-        )
 
 def teacher_course(request):
     if request.user.is_authenticated:
